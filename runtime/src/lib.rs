d49f8dc3e69776647477081ca643cc7a2b4d4201
mendozg <gabrielemf@hotmail.com>
2023-02-20 16:20:12 -0600

    remove gilt benchmark

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1832,7 +1832,6 @@ mod benches {
 		[pallet_election_provider_support_benchmarking, EPSBench::<Runtime>]
 		[pallet_elections_phragmen, Elections]
 		[pallet_fast_unstake, FastUnstake]
-		[pallet_gilt, Gilt]
 		[pallet_grandpa, Grandpa]
 		[pallet_identity, Identity]
 		[pallet_im_online, ImOnline]
